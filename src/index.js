2cf80e210cc28e9cd98b1ef7d22ed52b268f650f
Anthony Schneider <tonyschneider3@gmail.com>
2023-08-15 14:47:29 -0500

    init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import HomePage from "./pages/HomePage";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "@fontsource/roboto/300.css"; // Import the font for font-weight: 300
+import "@fontsource/roboto/400.css"; // Import the font for font-weight: 400
+import "@fontsource/roboto/500.css"; // Import the font for font-weight: 500
+import "@fontsource/roboto/700.css"; // Import the font for font-weight: 700
+import { ThemeContextProvider } from "./utils/ThemeContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import { Toaster } from "react-hot-toast";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <Toaster /> */}
+    <ThemeContextProvider>
+      <RouterProvider router={router} />
+    </ThemeContextProvider>
   </React.StrictMode>
 );
 
